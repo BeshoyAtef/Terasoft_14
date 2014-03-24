6b875da95002fea9e61a357e74e32e8c69f523d7
RanaLabib <rana.tlabib@gmail.com>
2014-03-24 21:30:28 +0200

    Create base project to build on it

---
+++ Shipme_proj/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShipmeProj::Application.config.secret_key_base = 'f6c392aa6abeb6940c2cb65aa6e2eb15bbb07d7025e37770beaba14f50019d75fc52ad50ed570111920ac38f883deefdbff57ba6c75c3d79d55ae3ebeb024101'
