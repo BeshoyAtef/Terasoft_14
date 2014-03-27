d85ccfe79c1742cf50b8b7b2d93154831fa6c69c
fady <fady@ubuntu.(none)>
2014-03-27 15:18:03 -0400

    name of the commit

--- Shipme_proj/config/routes.rb
+++ Shipme_proj/config/routes.rb
@@ -1,4 +1,7 @@
 ShipmeProj::Application.routes.draw do
+  resources :ideas
+
+  get "welcome/index"
   get "demo/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
