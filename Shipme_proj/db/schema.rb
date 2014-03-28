ba95fa602a6e32042b1be941cfba502f831cb5b4
RanaLabib <rana.tlabib@gmail.com>
2014-03-28 23:31:54 +0200

    Resolve conflict

--- Shipme_proj/db/schema.rb
+++ Shipme_proj/db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20140327235919) do
 
   create_table "users", force: true do |t|
@@ -30,7 +31,25 @@ ActiveRecord::Schema.define(version: 20140327235919) do
     t.integer  "mobileNumber"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+ActiveRecord::Schema.define(version: 20140327191646) do
+
+  create_table "requests", force: true do |t|
+    t.integer  "userid"
+    t.integer  "packageid"
+    t.boolean  "accepted"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "trips", force: true do |t|
+    t.string   "destination"
+    t.string   "location"
+    t.integer  "maxweight"
+    t.date     "depdate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 
 end
