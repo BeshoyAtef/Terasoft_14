cbbcded03d098da56cd7c2352aa12c89758cc52a
omarabdelfatah <omar.a.fatah@gmail.com>
2014-03-28 21:43:21 +0200

    Revert "calculate transactions"
    This reverts commit cb0301bc18348714d237c0cbf5d7d1d5fd25dad4.

--- Shipme_proj/app/controllers/compare_controller.rb
+++
@@ -1,4 +0,0 @@
-class CompareController < ApplicationController
-  def index
-  end
-end
--- Shipme_proj/config/routes.rb
+++ Shipme_proj/config/routes.rb
@@ -1,6 +1,4 @@
 ShipmeProj::Application.routes.draw do
-  get "compare/index"
-  get "count/index"
   get "demo/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
