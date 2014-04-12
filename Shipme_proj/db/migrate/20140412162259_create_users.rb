578a9da790ce81006224879e92a21b06eba0ba8c
YoussefBarakat <youssefbarakat93@gmail.com>
2014-04-12 20:22:02 +0200

    Add a database

---
+++ Shipme_proj/db/migrate/20140412162259_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :username
+      t.integer :mobileNumber
+      t.integer :creditCard
+      t.boolean :admin
+      t.integer :idNumber
+      t.boolean :idVerify
+      t.floats :averageRating
+      t.date :dateOfRegister
+      t.integer :user_Id
+
+      t.timestamps
+    end
+  end
+end
--- Shipme_proj/db/migrate/20140412163024_create_requests.rb
+++ Shipme_proj/db/migrate/20140412163024_create_requests.rb
@@ -1,9 +1,9 @@
 class CreateRequests < ActiveRecord::Migration
   def change
     create_table :requests do |t|
-      t.integer :userid
-      t.integer :packageid
-      t.boolean :accepted
+      t.boolean :accept
+      t.integer :carrier_Id
+      t.integer :sender_Id
 
       t.timestamps
     end
