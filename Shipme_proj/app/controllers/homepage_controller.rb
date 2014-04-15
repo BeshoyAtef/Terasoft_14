7fbc078cf467fad407810eb21675108f5ad03965
rehab <rehab.a.arafa@gmail.com>
2014-04-14 22:12:11 -0400

    fix master problem

--- Shipme_proj/app/controllers/homepage_controller.rb
+++ Shipme_proj/app/controllers/homepage_controller.rb
@@ -1,12 +1,6 @@
 class HomepageController < ApplicationController
 
-#This method calculates the total number of packages deliverd 
-#Output: @packages_count-int
-#Author :Rehab ELShahawy
-
- def index
- 		@packages_count= Packages.find(:all, :conditions => {:finalDelivery => true}).count
- 		
+ def index 		
  end	
 
 end
