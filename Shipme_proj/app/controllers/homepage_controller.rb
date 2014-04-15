152ed2f86da5323024f7b053e305bfb42f5acd3d
rehab <rehab.a.arafa@gmail.com>
2014-04-14 22:31:29 -0400

    count packages deliverd

--- Shipme_proj/app/controllers/homepage_controller.rb
+++ Shipme_proj/app/controllers/homepage_controller.rb
@@ -1,6 +1,12 @@
 class HomepageController < ApplicationController
 
- def index 		
+#This method calculates the total number of packages deliverd 
+#Output: @packages_count-int
+#Author :Rehab ELShahawy
+
+ def index
+ 		@packages_count= Packages.find(:all, :conditions => {:finalDelivery => true}).count 
+
  end	
 
 end
