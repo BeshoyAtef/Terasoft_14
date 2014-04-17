a7f49935a183da356c614f3206efda910efd463f
fady <fady@ubuntu.(none)>
2014-04-17 16:04:44 -0400

    add important info:id ,:mobile :creditcard

---
+++ Shipme_proj/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	has_many :messages, :class_name => "Users", :foreign_key => "user_Id"
+	has_many :notifications, :class_name => "Users", :foreign_key => "user_Id"
+	has_many :packages, :class_name => "Users", :foreign_key => "user_Id"
+	has_many :payment, :class_name => "Users", :foreign_key => "user_Id"
+	has_many :reports, :class_name => "Users", :foreign_key => "user_Id"
+	has_many :requests, :class_name => "Users", :foreign_key => "user_Id"
+	has_many :trips, :class_name => "Users", :foreign_key => "user_Id"
+end
