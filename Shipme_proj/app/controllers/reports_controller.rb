d3774c1e484b412f226eba9f2ba703382b4b991f
sharkas <sharkas23@gmail.com>
2014-04-21 10:49:13 +0200

    documentation and implementation handled

--- Shipme_proj/app/controllers/reports_controller.rb
+++ Shipme_proj/app/controllers/reports_controller.rb
@@ -1,12 +1,16 @@
 class ReportsController < ApplicationController
-  def index
-  end
- #
-  def createReport
+  
+
+#This method is taking text input from html file and adding it in the database files
+#Reports-string PackageNo-int
+#It dosent return it adds in the database
+#Mohamed Adly G
+
+ 	def CreateReport
   
   	@rep = Reports.new
-  	@rep.description = params[:requiredReport]
-  	@rep.package_Id =params[:requiredPackageNo]
+  	@rep.description = params[ :requiredReport ]
+  	@rep.package_Id = params[ :requiredPackageNo ]
   	@rep.save
   	redirect_to :action => 'index'
   end
