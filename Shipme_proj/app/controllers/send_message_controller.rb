cd35dced319f737652b9c792913f13fcca401d9b
madeleine aziz saad <madeleine@madeleines-MacBook-Pro.local>
2014-04-24 12:02:44 +0200

    modified send messages and view conversations

--- Shipme_proj/app/controllers/send_message_controller.rb
+++ Shipme_proj/app/controllers/send_message_controller.rb
@@ -1,36 +1,35 @@
 class SendMessageController < ApplicationController
- 
-  def view_conversation
-   @messages_sent= Messages.find(:all,
-    :conditions => ["sender_Id = ? and receiver_Id = ? or sender_Id = ? and receiver_Id = ? ",1,2,2,1], 
-    :order => 'created_at' )
-  @conv= @messages_sent
-
-end
-
+  
+ # this method opens the page to create a new message
+ #Author: Madeleine Aziz Saad
   def construct_message
-
   end
 
+#This method gets the conversation between the user signed in and another user 
+#Input params[:receiver_Id, :ender_Id]
+#Returns @conv-array[Messages]
+#Author Madeleine Aziz Saad
+  def view_conversation
+    $receiver_Id = params.shift.last()
+    $ender_Id = params.shift.last()
+    @messages_sent= Messages.find(:all,:conditions => ["senders_id = ? and receivers_id = ? or senders_id = ? 
+      and receivers_id = ? ",$receiver_Id,$sender_Id,$sender_Id,$receiver_Id],:order => 'created_at' )
+   
+    @conv= @messages_sent
+  end 
 
+#This method takes the input of the tect field and adds it in the Messages model and then redirect to the conversation page
+#Input params[:text, :receiver_Id, :ender_Id]
+#Author Madeleine Aziz Saad
   def send_now
-  	@sender_Id=1
-  	@receiver_Id=2
-  	@description= params[:text]
 
-    puts @description
+    @description= params[:text]
+   
+    @msg = Messages.new(:receivers_id => @receiver_Id,:senders_id => @sender_Id,
+  	:description => @description)
 
-  	@msg = Messages.new(
-  		:receiver_Id => @receiver_Id,
-  		:sender_Id => @sender_Id,
-  		:description => @description
-      )
-  		@msg.save
-
-       redirect_to(:action => 'view_conversation')
-
-
+    @msg.save
+    redirect_to(:action => 'view_conversation')
+  
   end
-
-
 end
