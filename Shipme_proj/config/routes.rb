be55a7a5ce01dba3d3d730b73a44f058f18a1188
joe <joe@ubuntu.ubuntu-domain>
2014-04-24 18:58:26 +0200

    registeration

--- Shipme_proj/config/routes.rb
+++ Shipme_proj/config/routes.rb
@@ -12,7 +12,6 @@ ShipmeProj::Application.routes.draw do
 #HEAD
 #HEAD
 resources :add_trips
-resources :create_packages
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
