899834e8fa5eed7d78180acafcef775c1ca975a0
manareltayeb <manareltayeb21@gmail.com>
2014-04-24 23:03:21 +0200

    Send requests to carrier comp

---
+++ Shipme_proj/app/controllers/send_requests_controller.rb
@@ -0,0 +1,34 @@
+class SendRequestsController < ApplicationController
+ 
+  #This method is inserting the requests being sent in table Requests in the db.
+  #input : params[:id].
+  #Author:  Manar A. Eltayeb.
+
+ def  requests
+       
+    if @requests_count < 3
+
+        request = Requests.new
+        request.carriers_id = params[ :cid ]
+        request.senders_id= cookies[:user_id ]
+        request.packages_id = params[ :pid ]
+        request.accept = false
+        request.save
+    end
+      
+  end
+  
+
+  #This method is counting the number of sent requests.
+  #input:params[:pid].
+  #output: number of packages.
+  #Author: Manar A. ElTayeb.
+
+  def  sendrequest
+       
+    @requests_count = Requests.find( :all, :conditions => { :packages_id => params[:pid]} ).count 
+
+
+  end
+end
+
