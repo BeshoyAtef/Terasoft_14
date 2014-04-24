016dff6ae78dd4383c3a7b49749033ccdaaf4661
omarabdelfatah <omar.a.fatah@gmail.com>
2014-04-24 16:03:28 +0200

    Show international prices controller and view

---
+++ Shipme_proj/app/controllers/compareprices_controller.rb
@@ -0,0 +1,9 @@
+# This method show the international prices
+# Input:
+# Output: show the international prices of other companies in view international prices button
+# Author: Omar A. Abdelfatah
+
+class ComparepricesController < ApplicationController
+  def compare
+  end
+end
