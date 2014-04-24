d1e429b2185ad917a93d637bd25d1150e0c0c7f5
manareltayeb <manareltayeb21@gmail.com>
2014-04-24 21:49:19 +0200

    Send requests to carriers

--- Shipme_proj/app/models/reports.rb
+++ Shipme_proj/app/models/reports.rb
@@ -1,4 +1,5 @@
 class Reports < ActiveRecord::Base
-	belongs_to :reporter, :class_name => "Users", :foreign_key => "reporter_Id"
-	belongs_to :reported, :class_name => "Users", :foreign_key => "reported_Id"
+	belongs_to :reporter, :class_name => "Users"
+	belongs_to :reported, :class_name => "Users"
+	belongs_to :packages
 end
