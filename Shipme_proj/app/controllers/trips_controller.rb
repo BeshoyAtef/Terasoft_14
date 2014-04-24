25fc22aa27d574fcea0ff13ad45d801c761dc9e7
ahmedheshamnasser <ahmed.hesham.nasser@gmail.com>
2014-04-24 18:26:45 +0200

    Documentation and implementation

--- Shipme_proj/app/controllers/trips_controller.rb
+++ Shipme_proj/app/controllers/trips_controller.rb
@@ -5,7 +5,7 @@ class TripsController < ApplicationController
 #This method return the attributes entered in the database.
 #Author: Ahmed H. Nasser.
 
-  def  complete_user
+  def  CompleteUser
     @comp = Users.find(params[:id])
     @com = @comp.id
     @comp.destroy
@@ -26,28 +26,28 @@ class TripsController < ApplicationController
 #This method returns the values inserted to the database.
 #Author: Ahmed H. Nasser.
 
-  def  create
+  def  Create
     @userId = Users.find(:all).last().id
 	  @ver = Users.find(@userId)
     if (@ver[:creditCard]== nil)
       redirect_to :action => 'complete' , :id => @userId
-    elsif (@ver[:mobileNumber]== nil)
-      redirect_to :action => 'complete' , :id => @userId
-    elsif (@ver[:idNumber] == nil)
-      redirect_to :action => 'complete' , :id => @userId
-    elsif  @ver[:idVerify] == true || @ver[:idVerify] != true
-		  @trip = Trips.new
-		  @trip.destination = params[:requireddestination]
-		  @trip.location = params[:requiredlocation]
-		  @trip.maxWeight = params[:requiredweight]
-		  @trip.travelDate = params[:requiredtraveldate]
-		if @trip.save
-		  redirect_to :action => 'index'  
-    else
-      redirect_to :action => 'index'
-  end
-    else
-      redirect_to :action => 'newtrips' 
-  end
-  end
-  end          
+        elsif (@ver[:mobileNumber]== nil)
+          redirect_to :action => 'complete' , :id => @userId
+            elsif (@ver[:idNumber] == nil)
+              redirect_to :action => 'complete' , :id => @userId
+                elsif  @ver[:idVerify] == true || @ver[:idVerify] != true
+            		  @trip = Trips.new
+            		  @trip.destination = params[:requireddestination]
+            		  @trip.location = params[:requiredlocation]
+            		  @trip.maxWeight = params[:requiredweight]
+            		  @trip.travelDate = params[:requiredtraveldate]
+		                if @trip.save
+		                  redirect_to :action => 'index'  
+                      else
+                      redirect_to :action => 'index'
+                    end
+                  else
+                  redirect_to :action => 'newtrips' 
+                end
+            end
+        end          
