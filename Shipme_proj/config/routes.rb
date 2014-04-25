0af26b7de953eb985917fd6b9f48504511cc7866
rehab <rehab.a.arafa@gmail.com>
2014-04-25 08:36:47 -0400

    sigin in

--- Shipme_proj/config/routes.rb
+++ Shipme_proj/config/routes.rb
@@ -1,18 +1,29 @@
 ShipmeProj::Application.routes.draw do
-#HEAD
-#HEAD
-  resources :add_trips
+ get "user/signup"
+ get "user/signin"
+ get "user/forgot"
+ get "user/create"
+ get "user/in"
+ get "user/forgot_mail"
+ get "user/used"
+ get "homepage/index"
+ get "user/logout"
 
- resources :create_packages
+#HEAD
+#HEAD
+resources :add_trips
+resources :create_packages
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'user#signin2'
+       
   # You can have the root of your site routed with "root"
-  root 'homepage#index'
+
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+   #root_path get 'create_packages/:id' => 'create_packages#index'
+  #   get 'create_packages/:format'=> 'create_packages#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
