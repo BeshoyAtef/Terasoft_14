de8b35dcbfda02132c0556d65481b17e11385b85
sharkas <sharkas23@gmail.com>
2014-04-25 20:21:19 +0200

    Reports with cookies id

--- Shipme_proj/app/controllers/reports_controller.rb
+++ Shipme_proj/app/controllers/reports_controller.rb
@@ -5,12 +5,15 @@ class ReportsController < ApplicationController
 #It dosent return it adds in the database
 #Mohamed A Gheith
 
+
   def CreateReport
   
     @rep = Reports.new
+    @rep.reporters_id =  cookies[ :user_id ] 
     @rep.description = params[ :requiredReport ]
-    @rep.package_Id = params[ :requiredPackageNo ]
-    @rep.save
-    redirect_to :action => 'index'
+    @rep.packages_id = params[ :requiredPackageNo ]
+    	if @rep.save
+    		redirect_to :action => 'index'
+		end
   end
 end
