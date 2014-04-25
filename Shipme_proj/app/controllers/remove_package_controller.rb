2ad601fa77e68d49f3b703d1a3bab46e64cfd15c
Your Name <you@example.com>
2014-04-26 00:48:50 +0200

    Delete package & send notification if package was accepted Update

--- Shipme_proj/app/controllers/remove_package_controller.rb
+++ Shipme_proj/app/controllers/remove_package_controller.rb
@@ -1,8 +1,9 @@
 class RemovePackageController < ApplicationController
   
-#This method get user_id and package_id and delete a certain package.
+#This method delete a certain package.
+#input : cookies[ :user_id int and package_id int
+#output: redirects to deletepackage Page "Package Deleted"
 #Author: Salah K. Zalat.
-
 
   def deletepackage
   	  @package=Packages.find(:all, :conditions => {:id => params[:id], :senders_id => cookies[ :user_id ]})
@@ -18,25 +19,37 @@ class RemovePackageController < ApplicationController
     end  	
     end
    return;
-  end 
+  end
+
+  #This method notify carrier if package getting delete was already accepted.
+  #Input: user id 
+  #Output: @notification sending notification to carrier if condition is satisfied.
+  #Author: Salah K. Zalat
+
 
   def notification (user_id)
-   requests=Requests.find(:all, :conditions => {:senders_id => cookies[ :user_id ]}) 
-   packages=Packages.find(:all, :conditions => {:senders_id => cookies[ :user_id ]}) 
-   if(requests != nil) 
-     packages.each do |t|
-     	requests.each do |s|
+   @requests=Requests.find(:all, :conditions => {:senders_id => cookies[ :user_id ]}) 
+   @packages=Packages.find(:all, :conditions => {:senders_id => cookies[ :user_id ]}) 
+   if(@requests != nil) 
+     @packages.each do |t|
+     	@requests.each do |s|
      		if ((t.id == s.packages_id) && (t.senders_id == s.senders_id) && (t.carriers_id == s. carriers_id) && (s.accept == true))
-		      notification = Notifications.new; 
-   		      notification.users_id = t.carriers_id; 
-   		      notification.description = "A package has been deleted"
-   		      notification.save;
+		      @notification = Notifications.new; 
+   		      @notification.users_id = t.carriers_id; 
+   		      @notification.description = "A package has been deleted"
+   		      @notification.save;
    		    end 
    	    end 
       end 
     end
    return; 
 end
+
+
+#This method Show the package on the website.
+#Input:
+#Output:
+#Author: Salah K. Zalat
 
    def show
   	 @package=Packages.find(params[:id])
