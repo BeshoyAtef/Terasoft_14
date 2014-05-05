23532b00e16b8a028935f20477ae2375bac3d35f
manareltayeb <manareltayeb21@gmail.com>
2014-05-05 19:03:40 +0200

    Send requests to carriers in model request

--- Shipme_proj/app/controllers/requests_controller.rb
+++ Shipme_proj/app/controllers/requests_controller.rb
@@ -1,11 +1,20 @@
 class RequestsController < ApplicationController
+
   def index
-  end
+    @requests_counter = Requests.count_requests( params[:pid] )
+    
+
+    end
 
   def new
   end
 
   def create
+    @requests_count = Requests.find( :all, :conditions => { :packages_id => params[:pid]} ).count
+   if( @requests_count < 3 )
+
+       Requests.send_requests( cookies[:user_id] , params[:cid] , params[:pid] )
+    
   end
 
   def edit
@@ -15,6 +24,8 @@ class RequestsController < ApplicationController
   end
 
   def list
+   
+  end
   end
 
   def show
--- Shipme_proj/app/models/requests.rb
+++ Shipme_proj/app/models/requests.rb
@@ -2,4 +2,29 @@ class Requests < ActiveRecord::Base
 	belongs_to :sender, :class_name => "Users"
 	belongs_to :carrier, :class_name => "Users"
 	belongs_to :packages
+
+	#This method is inserting the request sent by the user in the database.
+	#carrier_id , sender_id , package_id.
+	#Author:Manar A. Eltayeb.
+
+
+	def self.send_requests (carrier_id , sender_id , package_id)
+
+     request = Requests.new
+     request.carriers_id = carrier_id
+     request.senders_id= sender_id
+     request.packages_id = package_id
+     request.accept = false
+     request.save
+    end
+
+    #This method is counting the number of requests sent on a certain package.
+    #packages_id.
+    #@request_counter.
+    #Author:Manar A. Eltayeb.
+
+
+    def self.count_requests (packages_id)
+    	@requests_counter = Requests.find( :all, :conditions => { :packages_id => packages_id} ).count 
+    end
 end
