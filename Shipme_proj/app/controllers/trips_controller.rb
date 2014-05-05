3b4d4b05c10fe37787457c47915cf708bd76b7bd
Youssef <youssef.saleh.se@gmail.com>
2014-05-05 21:57:30 +0200

    Issue #118, edit the controller and the model according to the new conventions

--- Shipme_proj/app/controllers/trips_controller.rb
+++ Shipme_proj/app/controllers/trips_controller.rb
@@ -1,5 +1,7 @@
 class TripsController < ApplicationController
   def index
+    @current_user_id = cookies[:user_id]
+    @current_user_trips = Trips.find(:all, :conditions => {:users_id => @current_user_id})
   end
 
   def new
@@ -12,6 +14,19 @@ class TripsController < ApplicationController
   end
 
   def update
+    @current_user_id = cookies[:user_id]
+    @current_trip = Trips.find(params[ :id ])
+    @destination = params[ :required_destination]
+    @location = params[ :required_location ]
+    @travel_date = params[ :required_traveldate]
+    @max_weight = params[ :required_num_weight ]
+    data_validated = true
+    if (@destination.length == 0 or !(@destination =~ /\S/) or @location.length == 0 or !(@location =~ /\S/) or @max_weight == nil or !(@max_weight =~ /\S/)  or @travel_date == nil or !(@travel_date =~ /\S/))
+      data_validated = false
+    end
+    if( data_validated == true)
+    Trips.edit_the_trip(@current_trip,@destination,@location,@max_weight,@travel_date,@current_user_id)
+    end
   end
 
   def list
--- Shipme_proj/app/models/trips.rb
+++ Shipme_proj/app/models/trips.rb
@@ -1,3 +1,11 @@
 class Trips < ActiveRecord::Base
 	belongs_to :users
+	def self.edit_the_trip(trip,destination,location,maximum,travel,user)
+        trip.destination = destination
+        trip.location = location
+        trip.maxWeight = maximum
+        trip.travelDate = travel
+        trip.users_id = user
+        trip.save
+ 	end
 end
