433706790111221ccdabf086f8bf7ce324ce9db8
ahmedsamouka <ahmed_samouka@hotmail.com>
2014-05-07 17:08:33 +0200

    #145 create package story adjusted with CRUD method

--- Shipme_proj/app/controllers/packages_controller.rb
+++ Shipme_proj/app/controllers/packages_controller.rb
@@ -3,9 +3,22 @@ class PackagesController < ApplicationController
   end
 
   def new
+    @p = false
+    @user = Users.find( cookies[ :user_id ] ) #.last().id
+    @verify = @user.idVerify
+    if @verify != true
+      redirect_to :action => 'index'
+    else
+       
+    end
   end
 
   def create
+    Packages.create_package(cookies[:user_id], params[ :requireddestination ], params[ :requireddescription ],
+     params[ :requiredreceiverAddress ],params[ :requiredorigin ],params[ :requiredreceiverMobNumber ],
+     params[ :receiverEmail ],params[ :requiredweight ], params[ :requiredpackageValue ], params[:requiredexpiryDate])
+
+
   end
 
   def edit
@@ -15,9 +28,13 @@ class PackagesController < ApplicationController
   end
 
   def list
+    senders_id=cookies[:user_id]
+    #@create_packages = Packages.all
+    @create_packages=Packages.view_all_packages(senders_id)
   end
 
   def show
+    @pack = Packages.find( params[ :id ] ) 
   end
 
   def delete
--- Shipme_proj/app/models/packages.rb
+++ Shipme_proj/app/models/packages.rb
@@ -6,4 +6,23 @@ class Packages < ActiveRecord::Base
 	has_one :reports
 	has_many :requests
 
+
+	def self.view_all_packages(user_id)
+	  @create_packages = Packages.find( :all ,:conditions => {:senders_id => user_id })
+    end
+
+    def self.create_package(senders_id,destination,description,address,origin,mobnumber,email,weight,value,exdate)
+        @create_packages = Packages.new
+        @create_packages.senders_id=senders_id
+        @create_packages.destination =  destination
+        @create_packages.description =  description
+        @create_packages.receiverAddress =  address
+        @create_packages.origin =  origin
+        @create_packages.receiverMobNumber =  mobnumber
+        @create_packages.receiverEmail =  email
+        @create_packages.weight =  weight
+        @create_packages.packageValue = value
+        @create_packages.expiryDate=exdate
+        @create_packages.save
+   end
 end
