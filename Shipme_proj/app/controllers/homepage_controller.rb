66d63185c4b481d13ad3722e8f6744474b8a8072
omarabdelfatah <omar.a.fatah@gmail.com>
2014-05-10 23:13:35 +0200

    Show international Prices with documentation fixed

--- Shipme_proj/app/controllers/homepage_controller.rb
+++ Shipme_proj/app/controllers/homepage_controller.rb
@@ -9,6 +9,11 @@ class HomepageController < ApplicationController
 
  	end	
 
+# This method show the international prices
+# Input:
+# Output: show the international prices of other companies in view international prices button
+# Author: Omar A. Abdel-fatah
+
  	def list
  	end
 
