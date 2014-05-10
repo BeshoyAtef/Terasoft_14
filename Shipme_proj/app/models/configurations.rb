274b365ca23308b345659bbb6008f7a867fbfa69
user <user@ubuntu.ubuntu-domain>
2014-05-10 22:36:48 +0200

    #146 inserting comission

--- Shipme_proj/app/models/configurations.rb
+++ Shipme_proj/app/models/configurations.rb
@@ -1,2 +1,14 @@
 class Configurations < ActiveRecord::Base
+
+
+#This method enter the comission value received from the controller into the database
+#Author : Ahmed T. Mohamed
+
+  def self.create (comission)
+    @configurations  = Configurations.new( comission )
+    @configurations.save
+  end   
+
+  validates_presence_of :comission, :message => "must be provided"
+  validates :comission, numericality: { only_integer: true }
 end
