29c8bd9801bda341e77b225526d36bd0b6f6937b
joe <joe@ubuntu.ubuntu-domain>
2014-05-10 23:58:37 +0200

    resolve conflict in routes

--- Shipme_proj/config/routes.rb
+++ Shipme_proj/config/routes.rb
@@ -1,5 +1,4 @@
 ShipmeProj::Application.routes.draw do
-
   #new routes
   get "admin/index"
   get "admin/new"
@@ -19,6 +18,15 @@ ShipmeProj::Application.routes.draw do
   get "messages/show"
   get "messages/delete"
   get "messages/destory"
+  get "authentication/index"
+  get "authentication/new"
+  get "authentication/create"
+  get "authentication/edit"
+  get "authentication/update"
+  get "authentication/list"
+  get "authentication/show"
+  get "authentication/delete"
+  get "authentication/destory"
   get "reports/index"
   get "reports/new"
   get "reports/create"
@@ -101,87 +109,101 @@ ShipmeProj::Application.routes.draw do
   get "user_log/forget"
   get "user_log/log_out"
   get "user_log/used" 
-   #old routes
+
+  #old routes
+ get "user/signup"
+ get "user/signin"
+ get "user/forgot"
+ get "user/create"
+ get "user/in"
+ get "user/forgot_mail"
+ get "user/used"
  get "homepage/index"
+ get "user/logout"
  get "compareprices/compare"
- get "send_requests/requests" 
- get "/send_requests/requests" => 'send_requests#requests', :as => 'Send_button' 
- post "/send_requests/requests" 
- get "send_requests/count_requests" 
- post "/send_requests/count_requests" 
- get "send_requests/sent" 
- post "/send_requests/sent" 
- get "/send_requests/requsets" 
- get "/send_requests/sent_request" 
- post "/send_requests/sent_request" 
+ get "send_requests/requests"
+ get "/send_requests/requests" => 'send_requests#requests', :as => 'Send_button'
+ post "/send_requests/requests"
+ get "send_requests/count_requests"
+ post "/send_requests/count_requests"
+ get "send_requests/sent"
+ post "/send_requests/sent"
+ get "/send_requests/requsets"
+ get "/send_requests/sent_request"
+ post "/send_requests/sent_request"
  get "/send_requests/sendrequest"
- get "notification/index" 
- get "notification/show" 
+ get "notification/index"
+ get "notification/show"
  get "notification/test"
  post "notification/test"
- post "notification/index" 
+ post "notification/index"
  get "create_packages/new"
  post "create_packages/new"
- get "create_packages/index" 
- get "create_packages/show_package" 
+ get "create_packages/index"
+ get "create_packages/show_package"
  post "create_packages/index"
 
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'user_log#sign_in2'
+  root 'user_log#signin2'
        
   # You can have the root of your site routed with "root"
 
 
   # Example of regular route:
    #root_path get 'create_packages/:id' => 'create_packages#index'
-  #   get 'create_packages/:format'=> 'create_packages#index'
+  # get 'create_packages/:format'=> 'create_packages#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # resources :products
 
   # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
+  # resources :products do
+  # member do
+  # get 'short'
+  # post 'toggle'
+  # end
   #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # collection do
+  # get 'sold'
+  # end
+  # end
 
   # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  # resources :products do
+  # resources :comments, :sales
+  # resource :seller
+  # end
 
   # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  # resources :products do
+  # resources :comments
+  # resources :sales do
+  # get 'recent', on: :collection
+  # end
+  # end
 
   # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  # concern :toggleable do
+  # post 'toggle'
+  # end
+  # resources :posts, concerns: :toggleable
+  # resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # namespace :admin do
+  # # Directs /admin/products/* to Admin::ProductsController
+  # # (app/controllers/admin/products_controller.rb)
+  # resources :products
+  # end
 end
+
+
+
+
+
