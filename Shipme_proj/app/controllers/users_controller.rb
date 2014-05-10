e97f45fd5ddd865557066c0e6d8b3145fcb7497b
fady <fady@ubuntu.(none)>
2014-05-10 08:43:35 -0400

    verify and update my info

--- Shipme_proj/app/controllers/users_controller.rb
+++ Shipme_proj/app/controllers/users_controller.rb
@@ -12,17 +12,36 @@ class UsersController < ApplicationController
   end
 
   def update
-  end
+ @user=Users.find_by_id(cookies[:user_id])
+  if Users.exists?(:id =>cookies[:user_id] )
+  $mobileNumber=params[:mobileNumber];
+  $creditCard=params[:creditCard];
+  $idNumber=params[:idNumber];
+  @user.update(:mobileNumber => $mobileNumber,:creditCard => $creditCard,:idNumber=> $idNumber) 
+   @user.save
+
+end
+end
 
   def list
+  @users = Users.find(:all, :conditions => "users.idVerify IS NULL")
   end
 
   def show
   end
+
+
 
   def delete
   end
 
   def destory
   end
+def update_attribute
+@users = Users.scoped(:conditions => " users.idVerify IS NULL")
+$idVerify=params[:idVerify];
+@users.update_all(:idVerify=> true) 
+@users.each(&:save)
 end
+end 
+  
