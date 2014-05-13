d984cf55fb0d61e144a1daf9fbe58207ea25b66f
Mohamed Adly Gheith <sharkas23@gmail.com>
2014-05-13 19:16:22 +0300

    added dots to documentation

--- Shipme_proj/app/controllers/reports_controller.rb
+++ Shipme_proj/app/controllers/reports_controller.rb
@@ -1,20 +1,20 @@
 class ReportsController < ApplicationController
   def index
   end
-  #This method is intended to show id of package in the package field 
-  #Input : it takes the id of the logged in user 
-  #output : returns package id
-  #Mohamed A.Gheith
-
+  #This method is intended to show id of package in the package field .
+  #Input : it takes the id of the logged in user .
+  #output : returns package id.
+  #Author : Mohamed A.Gheith.
+  
   def new
    @packages = Packages.find(:all , :conditions => {:senders_id => cookies[:user_id]})
   end
 
 
-  #This method Creates a new Report
-  #Input : Requiredname - String,RequiredReport - String , RequiredPackageNo -String
-  #output : Saves report in database
-  #Mohamed A.Gheith
+  #This method Creates a new Report.
+  #Input : Requiredname - String,RequiredReport - String , RequiredPackageNo -String.
+  #output : Saves report in database.
+  #Author : Mohamed A.Gheith.
 
   def create
     Reports.create_report(cookies[:user_id],params[:requiredname],params[:requiredReport],params[:requiredPackageNo])
