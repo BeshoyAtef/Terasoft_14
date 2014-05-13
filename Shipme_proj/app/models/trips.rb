cfc57921a07fe6dcfc3dc88621638cea983c55d8
hagaressam <hagar.aly@student.guc.edu.eg>
2014-05-13 19:30:10 +0300

    Fixed Implementation

--- Shipme_proj/app/models/trips.rb
+++ Shipme_proj/app/models/trips.rb
@@ -2,22 +2,22 @@ class Trips < ActiveRecord::Base
 	belongs_to :users
 	
 
-  # this method is showing all my trips.
-  # Input :user_id:Integer.
-  # Author : Hagar E. Aly.
+  	# this method is showing all my trips.
+  	# Input :user_id:Integer.
+	 # Author : Hagar E. Aly.
 
 	def self.my_trips (user_id)
 		@trips = Trips.find(:all , :conditions => {:users_id => user_id})
 	end
   
 
-   # this method is showing the selected trip.
-   # Input :trip_id:Integer.
-   # Author : Hagar E. Aly.
+	 # this method is showing the selected trip.
+	 # Input :trip_id:Integer.
+   	# Author : Hagar E. Aly.
 
 	def self.find_trip (trip_id)
 		@trip = Trips.find(trip_id)
 	end 
 	
 	
-	end
+end
