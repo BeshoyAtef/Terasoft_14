764fc33d639bd9f9c9935379646cefc7a1e16eb8
sharkas <sharkas23@gmail.com>
2014-05-13 18:10:45 +0200

    place a claim/report

--- Shipme_proj/app/controllers/reports_controller.rb
+++ Shipme_proj/app/controllers/reports_controller.rb
@@ -1,11 +1,23 @@
 class ReportsController < ApplicationController
   def index
   end
+  #This method is intended to show id of package in the package field 
+  #Input : it takes the id of the logged in user 
+  #output : returns package id
+  #Mohamed A.Gheith
 
   def new
+   @packages = Packages.find(:all , :conditions => {:senders_id => cookies[:user_id]})
   end
 
+
+  #This method Creates a new Report
+  #Input : Requiredname - String,RequiredReport - String , RequiredPackageNo -String
+  #output : Saves report in database
+  #Mohamed A.Gheith
+
   def create
+    Reports.create_report(cookies[:user_id],params[:requiredname],params[:requiredReport],params[:requiredPackageNo])
   end
 
   def edit
--- Shipme_proj/app/models/reports.rb
+++ Shipme_proj/app/models/reports.rb
@@ -2,4 +2,25 @@ class Reports < ActiveRecord::Base
 	belongs_to :reporter, :class_name => "Users"
 	belongs_to :reported, :class_name => "Users"
 	belongs_to :packages
+
+
+  #This methd intends to create a new report in the database
+  #Input : Reported username -string, Description -string , Package id-int 
+  #output : saves data in database
+  #Mohamed A.Gheith 
+  def self.create_report(reporter_id,reported_username,description,package_id)
+  
+    report = Reports.new
+    report.reporters_id = reporter_id 
+    report.description = description
+    report.packages_id = package_id
+
+    @users=Users.find(:all)
+    @users.each do |u|
+     if(u.username==reported_username)
+    	report.reported_id=u.id
+     end	
+    end
+   report.save
+  end
 end
