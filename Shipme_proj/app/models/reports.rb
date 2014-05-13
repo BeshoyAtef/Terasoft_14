fce3f042cda179adc5c61c191051eda4d2528ac8
Mohamed Adly Gheith <sharkas23@gmail.com>
2014-05-13 19:17:21 +0300

    added dots to documentation

--- Shipme_proj/app/models/reports.rb
+++ Shipme_proj/app/models/reports.rb
@@ -4,17 +4,17 @@ class Reports < ActiveRecord::Base
 	belongs_to :packages
 
 
-  #This methd intends to create a new report in the database
-  #Input : Reported username -string, Description -string , Package id-int 
-  #output : saves data in database
-  #Mohamed A.Gheith 
+  #This methd intends to create a new report in the database.
+  #Input : Reported username -string, Description -string , Package id-int .
+  #output : saves data in database.
+  #Author : Mohamed A.Gheith. 
+  
   def self.create_report(reporter_id,reported_username,description,package_id)
   
     report = Reports.new
     report.reporters_id = reporter_id 
     report.description = description
     report.packages_id = package_id
-
     @users=Users.find(:all)
     @users.each do |u|
      if(u.username==reported_username)
