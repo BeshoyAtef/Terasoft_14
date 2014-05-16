940e72411a6c9a2a3396dc1a6db676ccf4becaaa
madeleine aziz saad <madeleine@madeleines-MacBook-Pro.local>
2014-05-16 12:47:17 +0200

    send message documentation added

--- Shipme_proj/app/controllers/messages_controller.rb
+++ Shipme_proj/app/controllers/messages_controller.rb
@@ -1,19 +1,33 @@
-
 class MessagesController < ApplicationController
-  
+  def index
+  end
+
   def new
-   Messages.send_new_message(cookies[ :user_id ],params[:username],params[:text])
   end
 
-  def show
-    @messages_received = Messages.get_last_message (cookies[ :user_id ])
-    @users = Users.all_users
+  def create
   end
 
-#This method calls the get_conversation action in messages model giving it the sender id and receiver id
-#Input params[:receiver_Id, :ender_Id]
-#Returns @conv-array[Messages]
-#Author Madeleine Aziz Saad
+ #This method shows the message view.
+ #Author: Madeleine A. Saad.
+
+  def edit
+  end
+
+#This method sends a message.
+#user1_id - int, user2_id - int.
+#Author Madeleine A. Saad.
+
+  def update 
+     $description= params[:text] 
+     @msg = Messages.send_message($receiver_Id,$sender_Id,$description) 
+     redirect_to(:action => 'list') 
+  end
+
+#This method lists the conversation.
+#user1_id - int, user2_id - int.
+#Returns @conv - array.
+#Author Madeleine A. Saad.
 
   def list 
     $sender_Id= params.shift.last() 
@@ -22,19 +36,12 @@ class MessagesController < ApplicationController
    
   end 
 
-#This method takes the input of the text field, adds it in the Messages model and then redirect to the conversation page
-#Input params[:text]
-#Author Madeleine Aziz Saad
-  def update 
-     $description= params[:text] 
-     @msg = Messages.send_message($receiver_Id,$sender_Id,$description) 
-     redirect_to(:action => 'list') 
+  def show
   end
 
- #This method opens the page to create a new message
- #Author: Madeleine Aziz Saad
-
-  def create
+  def delete
   end
 
+  def destory
+  end
 end
