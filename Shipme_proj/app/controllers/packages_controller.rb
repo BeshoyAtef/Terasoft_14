1859f9b5d79ef0837a865d3a5e28162ea02ce5e1
abdelrahmanyass <ahmed@abdelrahmanyasser.(none)>
2014-05-16 13:15:36 +0200

    Issue #59, controller,model,view

--- Shipme_proj/app/controllers/packages_controller.rb
+++ Shipme_proj/app/controllers/packages_controller.rb
@@ -10,7 +10,13 @@ class PackagesController < ApplicationController
   #Author:  Ahmed.M.Samouka
 
   def new
-    redirect_to :action => 'index'
+    @p = false
+    @user = Users.find( cookies[ :user_id ] ) #.last().id
+    @verify = @user.idVerify
+    if @verify != true
+      redirect_to :action => 'index'
+    else
+    end
   end
 
 
@@ -50,11 +56,21 @@ class PackagesController < ApplicationController
     @pack = Packages.find( params[ :id ] ) 
   end
 
-  def delete
+  #This method finds all the packages by calling method (find_packages) in the model by passing the parameter  
+  #package_id - Integer.
+  #Author: Abdelrahaman Y. Seoudy.
+
+  def  delete
+    @package = Packages.find_package(params[:id])
   end
 
-  def destory
+  def  destory
+    @package = Packages.find_package(params[:id])
+    @package.destroy
+    flash[:notice] = "package Deleted successfully"
+    redirect_to(:action => 'list')
   end
+
 
   def confirm_delivery
   end  
--- Shipme_proj/app/models/packages.rb
+++ Shipme_proj/app/models/packages.rb
@@ -49,4 +49,12 @@ require 'will_paginate/array'
       @packages = @packages.paginate( :page => page , :per_page => 10 ) 
     end
 
+  #This method search for a certain package using the ID.  
+  #package_id - Integer.
+  #Author: Abdelrahaman Y. Seoudy.
+
+  def  self.find_package(package_id)
+     @package = Packages.find(package_id)
+  end 
+
 end
