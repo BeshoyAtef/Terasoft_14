ae6687eed38292041771b53c0a6b0ac4186c7976
omarabdelfatah <omar.a.fatah@gmail.com>
2014-05-16 16:42:56 +0300

    Update send_sms_controller.rb
    edited implemetation

--- Shipme_proj/app/controllers/send_sms_controller.rb
+++ Shipme_proj/app/controllers/send_sms_controller.rb
@@ -1,12 +1,12 @@
 class SendSmsController < ApplicationController
 require 'twilio-ruby'
 
-#this method is made to send sms, by taking 2 inputs the method first intialize the connection with twillio then create sms using the inputs  
-#Input: number string , text string
-#Returns: sends a sms 
-#Author: Omar A. Abdel-fatah
+  #this method is made to send sms, by taking 2 inputs the method first intialize the connection with twillio then create sms using the inputs  
+  #Input: number string , text string.
+  #Returns: sends a sms.
+  #Author: Omar A. Abdel-fatah.
 
-  def  send_sms(number,text)
+  def  send_sms( number,text )
     account_sid = "AC9ec4b58090b478bc49c58aa6f3644cc7"
     auth_token = "79ba8ebb0bf8377302f735f853cd7006"
     client = Twilio::REST::Client.new account_sid, auth_token
@@ -19,17 +19,17 @@ require 'twilio-ruby'
   end
 
 
-#this method view the sms 
-#Author: Omar A. Abdel-fatah
+  #this method view the sms 
+  #Author: Omar A. Abdel-fatah
 
   def  message
   end
 
 
-#this method test the sending sms with the given number
-#Author: Omar A. Abdel-fatah
+  #this method test the sending sms with the given number
+  #Author: Omar A. Abdel-fatah
 
- def  test
-    send_sms("+20126699638","Hey, Welcome to Shipme Website ;)")
+  def  test
+    send_sms( "+20126699638","Hey, Welcome to Shipme Website ;)" )
   end
-  end
+end
