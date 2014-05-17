e1ffeb161e8cb9235aa078fd20c172416c840fba
joe <joe@ubuntu.ubuntu-domain>
2014-05-17 16:09:08 +0200

    #152 #52 send welcome sms and reset password

--- Shipme_proj/app/controllers/user_log_controller.rb
+++ Shipme_proj/app/controllers/user_log_controller.rb
@@ -1,13 +1,13 @@
 class UserLogController < ApplicationController
 
-#The method is sigining up
+#The method is sigining up.
 #Author: John W.Ghali
   
   def sign_up
   end
 
 
-#The method is redirecting to the signin
+#The method is redirecting to the signin.
 #Author: John W.Gahli
 
   def sign_in2
@@ -15,8 +15,8 @@ class UserLogController < ApplicationController
   end
 
 
-#The method is saving the input to the database
-#Input: email string- username string- password string- mobilenumber int
+#The method is saving the input to the database.
+#Input: email string- username string- password string- mobilenumber int.
 #Author: John W.Ghali
 
   def create
@@ -30,12 +30,12 @@ class UserLogController < ApplicationController
     if(@approve == false)
         redirect_to :action => 'used'
     else
-    @user=Users.create(params[:email],params[:username],params[:password],params[:mobilenum])
+    @user=Users.create(params[ :email ],params[ :username ],params[ :password ],params[ :mobilenum ])
     end
   end
 
 
-#The method is signing in. It redirects to the homepage with saving the user id by cookies
+#The method is signing in. It redirects to the homepage with saving the user id by cookies.
 #Author: John W.Ghali
 
   def sign_in
@@ -45,8 +45,8 @@ class UserLogController < ApplicationController
   end
 
 
-#The method is comparing the input with the database
-#Return: When the sigin is true it redirects me to hompage and when it is false it redirects to a wrong username or password page
+#The method is comparing the input with the database.
+#Return: No output but it redirects me to hompage and when it is false it redirects to a wrong username or password page.
 #Author: John W.Ghali
 
   def in
@@ -60,8 +60,8 @@ class UserLogController < ApplicationController
           if( v.encrypted_password == params[ :password ])
             cookies[ :user_id ] = v.id
             cookies[ :username ] = v.username
-            #this if condition redirect the user after logging in to the right home page
-            #if the user is an admin redirects to admin homepage otherwise redirects to normal homepage
+            #this if condition redirect the user after logging in to the right home page.
+            #if the user is an admin redirects to admin homepage otherwise redirects to normal homepage.
             #Author: Rehab A.Elshahawy
              if(v.admin == true)
               redirect_to :action => "index" ,:controller => "admin"
@@ -70,30 +70,37 @@ class UserLogController < ApplicationController
             return
           end
         end
+      end
     end
   end
-end
 end 
 
-#The method is resting the password to the user
-#Input: email string
-#Return: It redirects to a page that an email will be sent with the password or a pop message to enter a valid email
+
+#The method is resting the password to the user.
+#Input: email string.
+#Return: No output but it redirects to a page that an email will be sent with the password or a pop message to enter a valid email.
 #Author: John W.Ghali
 
   def forgot_mail
-    @var = Users.find( :all )
-    @salout = "Please enter a valid email, then try again"
-    @var.each do |v|
-      if (v.email == params[ :mail ])
-        @salout = "An email will be sent with your password to " + v.email + " in a few minutes"
-      end
-
+  @salout1 = "Please enter a valid email, then try again"
+  @salout2 = "A message will be sent to you with your password to in a few minutes"
+  @users = Users.find( :all )
+  @users.each do |u|
+    if u.email== params[ :email ]
+     @mobile=u.mobileNumber
+        if ( @mobile!=nil )
+          @mob = @mobile.to_s
+          @password = u.encrypted_password
+          @user1 ='+20'.to_s + @mob
+          SendSmsController.send_sms( @user1 , "your password is: " + @password )
+        end
+      end 
     end
   end
 
 
-#The method is logging out
-#Return: It redirects to the sign in page
+#The method is logging out.
+#Return: N0 output but it redirects to the sign in page.
 #Author: John W.Ghali
 
   def log_out
--- Shipme_proj/spec/models/users_spec.rb
+++ Shipme_proj/spec/models/users_spec.rb
@@ -1,2 +1,25 @@
 require 'spec_helper'
 require 'users'
+
+describe Users do
+
+  it "is invalid without an email" do
+    non_email_user = User.new(email: 'whatever')
+    expect(non_email_user).to_not be_valid
+  end
+
+  it "is invalid without a username" do
+   	non_username_user = User.new(username: 'whatever')
+    expect(non_username_user).to_not be_valid
+  end
+
+  it "is invalid without a mobilenumber" do
+    non_mobileNumber_user = User.new(mobileNumber: 'whatever')
+    expect(non_mobileNumber_user).to_not be_valid
+  end
+
+  it "is invalid without a price" do
+    non_password_user = User.new(encrypted_password: 'whatever')
+    expect(non_password_user).to_not be_valid
+  end
+end
