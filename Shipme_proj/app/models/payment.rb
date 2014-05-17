7c6e0532a32018f396532c35807c5a020d27d1e5
mariowadiee <mario_104_20@hotmail.com>
2014-05-17 16:41:46 +0300

    Withdraw amount from creditcard and send notification #222

--- Shipme_proj/app/models/payment.rb
+++ Shipme_proj/app/models/payment.rb
@@ -1,4 +1,16 @@
 class Payment < ActiveRecord::Base
 	belongs_to :users
 	belongs_to :packages
+
+
+#This method is to call the controller method update to redirect add the withdraw money in amountWithdraw. 
+#inputs: no inputs
+#output :amountWithdraw int.
+#Author : Mario M. wadiee.
+
+	def self.update(payment,value,user_id,withdraw)
+		Payment.find( :all, :conditions => :amountWithdraw => withdraw)
+		payment.amountWithdraw => value)
+		payment.users_id => user_id)        
+	end	
 end
